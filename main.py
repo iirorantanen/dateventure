60e4d0e015696b1280d71ed49e87953e760cae05
Asteriski <asteriski@kekkonen.(none)>
2010-07-29 11:37:29 +0300

    Ilmoituksen muokkaus toimii.

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ class Showomat_ilmoitukset(webapp.RequestHandler):
           path=os.path.join(os.path.dirname(__file__),'omat_ilmoitukset.html')
           self.response.out.write(template.render(path,template_values))
 	  
-# This will review the details and ask the user if they really want to delete the date ilmoitus.
+# This will review the details and ask the user if they really want to delete the date announcement.
 class confirmDelete(webapp.RequestHandler):
     def post(self):
         user = users.get_current_user()
@@ -140,8 +140,69 @@ class confirmDelete(webapp.RequestHandler):
 	    template_values = {"url":users.create_logout_url("/"), 'datetime':ilmoitusVar.Datetime, 'location':ilmoitusVar.Paikka, 'description':ilmoitusVar.Kuvaus, 'key':postKey }
 	    self.response.out.write(template.render(path,template_values))
 
+# Modify the announcement -page
+class modify(webapp.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+	if not user:
+	    self.redirect(users.create_login_url(self.request.uri))
+	else: 
+	    path=os.path.join(os.path.dirname(__file__),'modify.html')	
+	    postKey = self.request.get('key')
+	    ilmoitusVar = Model.get(postKey)
+	    date = str(ilmoitusVar.Datetime.day) + "." + str(ilmoitusVar.Datetime.month) + "." + str(ilmoitusVar.Datetime.year)
+	    template_values = {"url":users.create_logout_url("/"), 'Age':ilmoitusVar.Age, 'Olen':ilmoitusVar.Olen, 'Etsin':ilmoitusVar.Etsin, 'location':ilmoitusVar.Paikka, 'description':ilmoitusVar.Kuvaus, 'key':postKey, 'hour':ilmoitusVar.Datetime.hour, 'minute':ilmoitusVar.Datetime.minute, 'date':date}
+	    self.response.out.write(template.render(path,template_values))
 
-# This will delete the ilmoitus
+# modifies the announcement
+class modifyAction (webapp.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+        ilmoitusVar = Model.get(self.request.get('key'))
+
+	ilmoitusVar.Ilmoittaja = user
+        OlenTemp =self.request.get('Olen')
+        ilmoitusVar.Olen = self.request.get('Olen')
+	etsinTemp = self.request.get('Etsin')
+	ilmoitusVar.Etsin = etsinTemp
+        PaikkaTemp = self.request.get('Paikka')
+        if PaikkaTemp!="":
+            ilmoitusVar.Paikka = self.request.get('Paikka')
+
+        ilmoitusVar.Time = datetime.time(int(self.request.get('Hour')), int(self.request.get('Min')))
+
+	DateTemp = self.request.get('Date')
+	Date_res = DateTemp.split('.')
+	ilmoitusVar.Date = datetime.date(int(Date_res[2]), int(Date_res[1]), int(Date_res[0]))
+	ilmoitusVar.Datetime = datetime.datetime(int(Date_res[2]), int(Date_res[1]), int(Date_res[0]), int(self.request.get('Hour')), int(self.request.get('Min')))
+
+#        Datetimetemp = self.request.get('Datetime')
+#        if Datetimetemp!="":
+#            DatetimeTemp = self.request.get('Datetime')
+#            Datetime_res = DatetimeTemp.split()
+#            Datetime_dt = Datetime_res[0]
+#            Datetime_mnth = Datetime_res[1]
+#            Datetime_yr = Datetime_res[2]
+#            Datetime_selTime = Datetime_res[3]
+#            Datetime_finalRes = Datetime_selTime.split(':')
+#            Datetime_hr = Datetime_finalRes[0]
+#            Datetime_min = Datetime_finalRes[1]
+#            Datetime_sec = Datetime_finalRes[2]
+#            ilmoitusVar.Datetime = datetime.datetime(int(Datetime_yr),int(Datetime_mnth),int(Datetime_dt),int(Datetime_hr),int(Datetime_min))
+        KuvausTemp = self.request.get('Kuvaus')
+        if KuvausTemp!="":
+            ilmoitusVar.Kuvaus = self.request.get('Kuvaus')
+        VastattuTemp =self.request.get('Vastattu')
+        if VastattuTemp!="":
+            ilmoitusVar.Vastattu = int(VastattuTemp)
+	AgeTemp = self.request.get('Age')
+	if AgeTemp != "":
+	  ilmoitusVar.Age = int(self.request.get('Age'))
+
+        ilmoitusVar.put()
+        self.redirect('/modified')
+
+# This will delete the announcement
 class delete(webapp.RequestHandler):
     def post(self):
         user = users.get_current_user()
@@ -295,6 +356,13 @@ class kuittaus(webapp.RequestHandler):
         template_values = {"url":users.create_logout_url("/")}
 	self.response.out.write(template.render(path,template_values))
 
+class modified(webapp.RequestHandler):
+    def get(self):
+	user = users.get_current_user
+        path=os.path.join(os.path.dirname(__file__),'modified.html')
+        template_values = {"url":users.create_logout_url("/")}
+	self.response.out.write(template.render(path,template_values))
+
 class ukk(webapp.RequestHandler):
     def get(self):
 	path=os.path.join(os.path.dirname(__file__),'ukk.html')
@@ -339,6 +407,9 @@ def main():
 		('/kiitos', kiitos),
 		('/kuittaus',kuittaus),
 		('/ukk', ukk),
+		('/modify', modify),
+		('/modifyaction', modifyAction),
+		('/modified', modified),
 		('/confirm', vahvistaIlmoituksenAvaus),
 		('/confirmed', ilmoitusVahvistettu),
                 ('/addilmoitus',ilmoitusAction),
